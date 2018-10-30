fe3d2b827707d649bee98ef68b028e6fb536072c
caiziruo <ziruocai@hotmail.com>
2018-10-30 15:03:02 +0800

    update

--- Matrix.hpp
+++ Matrix.hpp
@@ -23,6 +23,9 @@ public:
     double ** matrix;
     double ** factorization_L;
     double ** factorization_U;
+    double ** factorization_Q;
+    double ** factorization_R;
+
     vector<int> permutation_index;
     
     Matrix(int m, int n) { // Construction function, construct a mxn matrix.
@@ -37,9 +40,11 @@ public:
         
         factorization_L = NULL;
         factorization_U = NULL;
+        factorization_Q = NULL;
+        factorization_R = NULL;
     }
     
-    Matrix(Matrix const & B) { // Construction function, construct a existed matrix.
+    Matrix(Matrix const & B) { // Construction function, construct from the existed matrix B.
         row_dimension = B.row_dimension;
         column_dimension = B.column_dimension;
         
@@ -54,13 +59,33 @@ public:
         
         factorization_L = NULL;
         factorization_U = NULL;
+        factorization_Q = NULL;
+        factorization_R = NULL;
+    }
+
+    Matrix(double ** & pointer_A, int m, int n) { // Construction function, constructed from array pointer_A.
+        row_dimension = m;
+        column_dimension = n;
+
+        // Initialize matrix and copy B
+        matrix = new double * [row_dimension];
+        for (int i = 0; i < row_dimension; i++) {
+            matrix[i] = new double[column_dimension];
+            for (int j = 0; j < column_dimension; j++) {
+                matrix[i][j] = pointer_A[i][j];
+            }
+        }
+        
+        factorization_L = NULL;
+        factorization_U = NULL;
+        factorization_Q = NULL;
+        factorization_R = NULL;
     }
     
     void Set_Element(int i, int j, double x) {matrix[i][j] = x;}
     // double Matrix_element(int i, int j) const {return matrix[i][j]; }
     // int Matrix_row_dimension() const {return row_dimension;}
     // int Matrix_column_dimension() const {return column_dimension;}
-    // void copy_matrix(Matrix const B); // copy matrix B to this matrix
     
     void Interchange_row(int m, int n);
     
@@ -69,18 +94,22 @@ public:
     void Generate_Nonsingular();
     void Generate_Positive_Definite();
     void Generate_Diagonally_Dominant();
-    
-    void LU_factorization(bool use_pivoting);
-    double Norm(int p) const;
+
     double Frobenius_norm() const;
     
     void Print_Permutation() const;
     void Print_matrix() const;   // Print all the elements of the matrix
     void Print_L() const;
     void Print_U() const;
+
+    void LU_factorization(bool use_pivoting);
+    void QR_factorization(bool use_pivoting);
     Matrix Transpose() const;
+    Matrix inverse_upper_triangular(); 
     Matrix LU_factorization_L();
     Matrix LU_factorization_U();
+    Matrix QR_factorization_Q();
+    Matrix QR_factorization_R();
     
     // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     Matrix operator+(const Matrix & B) {
@@ -172,13 +201,6 @@ Matrix Matrix::Transpose() const {
     }
     
     return tmp;
-}
-
-double Matrix::Norm(int p) const {
-    double norm = 0;
-    // to be continued....
-    
-    return norm;
 }
 
 double Matrix::Frobenius_norm() const {
@@ -289,6 +311,27 @@ void Matrix::Generate_Diagonally_Dominant() {
     }
 }
 
+Matrix Matrix::inverse_upper_triangular() {
+    Matrix tmp(column_dimension, column_dimension);
+    tmp.Generate_Identity();
+
+    for (int i = column_dimension - 2; i >= 0; i--) {
+        for (int j = i + 1; j < column_dimension; j++) {
+            double aj = matrix[i][j] / matrix[j][j];
+            for (int k = j; k < column_dimension; k++) {
+                tmp.matrix[i][k] -= aj * tmp.matrix[j][k];
+            }
+        }
+    }
+    for (int i = 0; i < column_dimension; i++) {
+        for (int j = i; j < column_dimension; ++j) {
+            tmp.matrix[i][j] = tmp.matrix[i][j] / matrix[i][i];
+        }
+    }
+
+    return tmp;
+}
+
 Matrix Matrix::LU_factorization_L() {
     Matrix tmp(row_dimension, column_dimension);
     
@@ -307,6 +350,30 @@ Matrix Matrix::LU_factorization_U() {
     for (int i = 0; i < row_dimension; i++) {
         for (int j = 0; j < column_dimension; j++) {
             tmp.matrix[i][j] = factorization_U[i][j];
+        }
+    }
+    
+    return tmp;
+}
+
+Matrix Matrix::QR_factorization_Q() {
+    Matrix tmp(row_dimension, column_dimension);
+    
+    for (int i = 0; i < row_dimension; i++) {
+        for (int j = 0; j < column_dimension; j++) {
+            tmp.matrix[i][j] = factorization_Q[i][j];
+        }
+    }
+    
+    return tmp;
+}
+
+Matrix Matrix::QR_factorization_R() {
+    Matrix tmp(row_dimension, column_dimension);
+    
+    for (int i = 0; i < row_dimension; i++) {
+        for (int j = 0; j < column_dimension; j++) {
+            tmp.matrix[i][j] = factorization_R[i][j];
         }
     }
     
@@ -395,80 +462,139 @@ void Matrix::LU_factorization(bool use_pivoting) {
     }
 }
 
+void Matrix::QR_factorization(bool use_pivoting) {
+    Matrix copy_A(matrix, row_dimension, column_dimension);  //  R
+    Matrix copy_Q(copy_A); //  Q 
+    copy_Q.Generate_Identity();
 
-///////////////////////////////////////////////////////////////////////////////////////////
+    for (int i = 0; i < column_dimension; i++) {  // QR factorization. Q and R are stored in copy_A.
+        // int biggest_pivot_index = i;
+        // if (use_pivoting) {
+        //     double biggest_pivot = abs(copy_A.matrix[i][i]);
+        //     for (int j = i + 1; j < column_dimension; j++) { // find the biggest pivot
+        //         if (abs(copy_A.matrix[j][i]) > biggest_pivot) {
+        //             biggest_pivot_index = j;
+        //             biggest_pivot = abs(copy_A.matrix[j][i]);
+        //         }
+        //     }
+        // }
+        // else {
+        //     int biggest_pivot_index = i;
+        //     if (copy_A.matrix[i][i] == 0) {
+        //         for (int j = i + 1; j < column_dimension; j++) {
+        //             if (copy_A.matrix[j][i] != 0){
+        //                 biggest_pivot_index = j;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        
+        // //  interchange the rows of copy_A and copy_b if needed.
+        // if (biggest_pivot_index != i) {
+        //     double tmp;
+        //     for (int k = i; k < column_dimension; k++) {
+        //         tmp = copy_A.matrix[i][k]; // interchange copy_A
+        //         copy_A.matrix[i][k] = copy_A.matrix[biggest_pivot_index][k];
+        //         copy_A.matrix[biggest_pivot_index][k] = tmp;
+        //     }
+        // }
+        
+        // Q R factorization and record Q and R
+        double alpha = 0;
+        for (int j = i; j < row_dimension; j++) {
+            alpha += pow(copy_A.matrix[j][i], 2);
+        }
+        alpha = pow(alpha, 0.5);  
+
+        if (copy_A.matrix[i][i] > 0) {alpha = -alpha;} // to avoid cancellation
 
 
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //   v_i = copy_A[i:m][i] - alpha * e_i
+        Matrix v_i(row_dimension, 1);
+
+        copy_Q.matrix[i][i] = copy_A.matrix[i][i] - alpha;
+        v_i.matrix[i][0] = copy_Q.matrix[i][i];
+        for (int j = 0; j < i; j++) {
+            v_i.matrix[j][0] = 0;
+        }
+        for (int j = i + 1; j < row_dimension; j++) { // record v_{i} and Q[][i]
+            copy_Q.matrix[j][i] = copy_A.matrix[j][i];
+            v_i.matrix[j][0] = copy_A.matrix[j][i];
+        }
+
+        double v_i_norm  = pow(copy_Q.matrix[i][i], 2);  // compute the norm of v_{i}
+        for (int j = i + 1; j < row_dimension; j++) {      // compute the norm of v_{i}
+            v_i_norm += pow(copy_Q.matrix[j][i], 2);
+        }
+        // v_i_norm = pow(v_i_norm, 0.5);
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// int main() {
-    
-    
-    
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // int m = 21;
-    // int n = 12;
-    
-    // Linear_system linear_equation(m, n); // Initialize a mxn linear system for least square problem.
-    // for (int i = 0; i < m; i++) { // Set A
-    //     for (int j = 0; j < n; j++) {
-    //         linear_equation.Set_A_element(i, j, pow(i / (m - 1.0), j));
-    //     }
-    // }
+        // compute H{i} a_{i}
+        copy_A.matrix[i][i] = alpha;
+        for (int j = i + 1; j < row_dimension; j++) {
+            copy_A.matrix[j][i] = 0;
+        }
 
-    // Matrix initial_solution(n, 1);
-    // for (int i = 0; i < n; i++) {
-    //     initial_solution.Set_Element(i, 0, 1);
-    // }
+        // Hi = I - 2  v_{i} * v_{i}^{T}  / || v_{i} ||_{2}^{2}
+        Matrix old_a_i(row_dimension, 1); // old_a_i to record the old a_{k}
+        Matrix new_a_i(row_dimension, 1);
+        for (int k = i + 1; k < column_dimension; k++) { //  compute  H_{i} a_{k} 
+            for (int j = 0; j < row_dimension; j++) {
+                old_a_i.matrix[j][0] = copy_A.matrix[j][k];
+            }
 
-    // for (int i = 0; i < m; i++) { // Set b
-    //     double y_i = 0;
-    //     for (int j = 0; j < n; j++) {
-    //         y_i += linear_equation.Matrix_A_element(i, j);
-    //     }
-    //     linear_equation.Set_b_element(i, y_i);
-    // }
-    // // linear_equation.Show_system();
-    // // cout << '\n';
+            // Hi a_{k} = a_{k} - 2  v_{i} * v_{i}^{T} * a_{k}  / || v_{i} ||_{2}^{2}
+            new_a_i = old_a_i - v_i * v_i.Transpose() * old_a_i * 2 * (1.0 / v_i_norm);
+            for (int j = 0; j < row_dimension; j++) {
+                copy_A.matrix[j][k] = new_a_i.matrix[j][0];
+            }
+        }
+    }
 
-    // double perturbation_epsilon = pow(10, -10);
-    // for (int i = 0; i < m; i++) {
-    //     double tmp = (double)rand()/ RAND_MAX;
-    //     linear_equation.Set_b_element(i, linear_equation.Vector_b_element(i) + (2 * tmp - 1) * perturbation_epsilon);
-    // }
+    Matrix reduced_Q(row_dimension, column_dimension);  // Q: mxn
+    Matrix reduced_R(column_dimension, column_dimension); // R: nxn
+    Matrix inverse_R(column_dimension, column_dimension);
+    inverse_R.Generate_Identity();
+    for (int i = 0; i < column_dimension; ++i) {
+        for (int j = 0; j < column_dimension; ++j) {
+            reduced_R.matrix[i][j] = copy_A.matrix[i][j];
+        }
+    }
+
+    inverse_R = reduced_R.inverse_upper_triangular();
+
+    // compute Q = A * R^{-1}
+    Matrix original_A(matrix, row_dimension, column_dimension);
+    reduced_Q = original_A * inverse_R;
 
 
-    // Linear_system normal_equation_method(n, n);
-    // Matrix A(m, n);
-    // Matrix AT(n, m);
-    // Matrix ATA(n, n);
-    // Matrix ATb(n, 1);
-    // Matrix perturbated_b(m, 1);
+    if (factorization_Q == NULL) {
+        factorization_Q = new double * [row_dimension]; 
+        for (int i = 0; i < row_dimension; i++) {
+            factorization_Q[i] = new double [column_dimension]; 
+        }
+    }
+    for (int i = 0; i < row_dimension; i++) {
+        for (int j = 0; j < column_dimension; j++) {
+            factorization_Q[i][j] = reduced_Q.matrix[i][j];
+        }
+    }
 
-    // A = linear_equation.Matrix_A();
-    // AT = linear_equation.Matrix_A().Transpose();
-    // ATA = AT * AT.Transpose();                 //  define matrix A.T * A
-    // normal_equation_method.Set_A(ATA);
-    // perturbated_b = linear_equation.Vector_b();
+    if (factorization_R == NULL) {
+        factorization_R = new double * [column_dimension];
+        for (int i = 0; i < column_dimension; i++) {
+            factorization_R[i] = new double [column_dimension];
+        }
+    }
+    for (int i = 0; i < column_dimension; i++) {
+        for (int j = 0; j < column_dimension; j++) {
+            factorization_R[i][j] = reduced_R.matrix[i][j];
+        }
+    }
 
-    // ATb = AT * perturbated_b;
-    // normal_equation_method.Set_b(ATb);
-
-    // // normal_equation_method.Show_system();
-    // normal_equation_method.Solve_From_Cholesky();
-    // cout << "Normal solution method(Cholesky):\n";
-    // normal_equation_method.Show_Solution();
-    // cout << '\n';
-
-    // linear_equation.Solve_From_QR();
-    // cout << "Solution using QR factorization:" << "\n";
-    // linear_equation.Show_Solution();
-    // // cout << '\n';
-    // // linear_equation.Show_system();
-    
-
-    
-//     return 0;
-// }
+}
 
 #endif

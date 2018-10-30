108ac04bfef2e18c2e0b09ee6f8cd4f21b74f301
caiziruo <ziruocai@hotmail.com>
2018-10-30 18:20:39 +0800

    update

--- Eigenvalue.hpp
+++ Eigenvalue.hpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <string>
 
 using namespace std;
 
@@ -29,31 +30,78 @@ public:
 		else {dimension = m; }
 
 		original_A = new Matrix(matrix_A);
-		eigenvalues = NULL;
+		eigenvalues = new Matrix(dimension, 1);
 		eigenvectors = new Matrix(dimension, dimension);
-		
-		preliminary_reduction = false;
-    }
 
-    void power_iteration();
-    void QR_iteration();
-    void Preliminary_Reduction();
-    ~Eigenvalue() {
-        delete original_A;
-        delete eigenvalues;
-        delete eigenvectors;
-    }
+		preliminary_reduction = false;
+	}
+
+	void Get_eigenvalues(string method);
+	void power_iteration(int iterations);
+	void QR_iteration(int iterations);
+	void Preliminary_Reduction();
+
+	void Print_eigenvalues() const;
+	void Print_eigenvectors() const;
+
+	~Eigenvalue() {
+		delete original_A;
+		delete eigenvalues;
+		delete eigenvectors;
+	}
 
 };
 
-void Eigenvalue::QR_iteration() {
+void Eigenvalue::Get_eigenvalues(string method) {
+	if (method == "QR_iteration") {
+		QR_iteration(1000);
+	}
+}
+
+void Eigenvalue::Print_eigenvalues() const {
+	for (int i = 0; i < dimension; ++i) {
+		cout << eigenvalues -> matrix[i][0] << '\n';
+	}
+} 
+
+void Eigenvalue::Print_eigenvectors() const {
+	for (int i = 0; i < dimension; ++i) {
+		for (int j = 0; j < dimension; ++j) {
+			cout << setw(14) <<  eigenvectors -> matrix[i][j];
+		}
+		cout << '\n';
+	}
+}
+
+void Eigenvalue::QR_iteration(int iterations) {
+	Matrix Ak(dimension, dimension); 
+	Matrix eigenvectors_matrix(dimension, dimension);
+	eigenvectors_matrix.Generate_Identity();
+
 	if (preliminary_reduction == true) {
 
 	}
 	else {
-
+		Ak = (* original_A); 
 	}
 
+	Matrix Q(dimension, dimension);
+	Matrix R(dimension, dimension);
+
+	for (int i = 0; i < iterations; ++i) {
+		if (i % 10 == 0) {Ak.Matrix_approximation(1e-10); }
+
+		Ak.QR_factorization();
+		Q = Ak.QR_factorization_Q();
+		R = Ak.QR_factorization_R();
+		Ak = R * Q;
+		eigenvectors_matrix = eigenvectors_matrix * Q; 
+	}
+
+	for (int i = 0; i < dimension; i++) {
+		eigenvalues -> matrix[i][0] = Ak.matrix[i][i];
+	}
+	*eigenvectors = eigenvectors_matrix;
 }
 
 
--- QR_iteration_test.cpp
+++ QR_iteration_test.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 #include "Matrix.hpp"
+#include "Eigenvalue.hpp"
 
 
 int main() {
@@ -16,33 +17,17 @@ int main() {
     int n = 9;
     Matrix A(n, n);
     // A.Generate_Nonsingular();
-    A.Generate_Positive_Definite();
-    // A.Generate_Diagonally_Dominant();
+    // A.Generate_Positive_Definite();
+    A.Generate_Diagonally_Dominant();
 
-    Matrix eigenvectors(n, n);
-    eigenvectors.Generate_Identity();
-    Matrix Q(n, n);
-    Matrix R(n, n);
-    
 
-    // A.Print_matrix();
-    Matrix Ak(A);
-    cout << '\n';
-    for (int i = 0; i < 1000; ++i) {
-        if (i % 10 == 0) {
-            Ak.Matrix_approximation(1e-10);
-        }
-        Ak.QR_factorization(true);
-        Q = Ak.QR_factorization_Q();
-        R = Ak.QR_factorization_R();
-        Ak = R * Q;
-        eigenvectors = eigenvectors * Q;
-    }
 
-    Ak.Print_matrix();
-    // cout << '\n';
-    // A = (eigenvectors.Transpose() * A) * eigenvectors;
-    // A.Matrix_approximation(1e-10);
-    // A.Print_matrix();
+    Eigenvalue eigenvalues_computation(A, 9, 9);
+    eigenvalues_computation.QR_iteration(1000);
+
+    cout << "Eigenvalues:\n";
+    eigenvalues_computation.Print_eigenvalues();
+    cout << "\nEigenvectors:\n";
+    eigenvalues_computation.Print_eigenvectors();
 
 }
